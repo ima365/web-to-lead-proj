2c1f042fc77220ffcd9fe750111c2cc120c62ac5
Muda <mudashiruibrahim7@gmail.com>
2024-12-24 12:01:41 -0500

    Recaptcha chnages

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ function beforesubmit() {
     // set the formatted date into the second variable
     outputdate.value = formattedDate;
 
+    function timestamp() {
+        var response = document.getElementById("g-recaptcha-response");
+        if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+        }
+    }
+    setInterval(timestamp, 500); 
+
+
 }
